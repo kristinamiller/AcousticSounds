280d57a9b55c353147bd041588b57223a2a8d6b6
kristinamiller <kfm287@gmail.com>
2019-09-11 14:01:51 -0700

    finished user auth functionality

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
 
 const App = () => (
   <div>
+    {/* <Modal /> */}
     <header>
       <h1>Acoustic Sounds</h1>
       <GreetingContainer />
     </header>
-    {/* <Switch> */}
+    <Switch>
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
       {/* <Route exact path="/" component={Home}/> */}
-    {/* </Switch> */}
+    </Switch>
   </div>
 )
 
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 class SessionForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       username: "",
       password: ""
     }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
   }
 
   handleSubmit(e) {
@@ -16,42 +18,61 @@ class SessionForm extends React.Component {
     this.props.processForm(user);
   }
 
-  handleInput(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
+  handleEmail(e) {
+    this.setState({email: e.target.value})
+  }
+  handlePassword(e) {
+    this.setState({password: e.target.value})
+  }
+  
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    )
   }
 
 
   render() {
     return(
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <h3>{this.props.formType}</h3>
+      <div className="login-form-container">
+        <form onSubmit={this.handleSubmit} className="login-form-box">
+          <h3>Please {this.props.formType}</h3>
           <br/>
-          <label>
-            Email:
-            <input 
-              type="text"
-              value={this.state.email}
-              onChange={this.handleInput('email')}
-              />
-          </label>
-          <br/>
-          <br/>
-          <label>
-            Password:
-            <input 
-              type="password"
-              value={this.state.password}
-              onChange={this.handleInput('password')}
-              />
-          </label>
-          <br/>
-          <input type="submit" value={this.props.formType}/>
+          <h3>{this.renderErrors()}</h3>
+          <div className="login-form">
+            <br/>
+            <label>
+              Email:
+              <input 
+                type="text"
+                value={this.state.email}
+                onChange={this.handleEmail}
+                className="login-input"
+                />
+            </label>
+            <br/>
+            <br/>
+            <label>
+              Password:
+              <input 
+                type="password"
+                value={this.state.password}
+                onChange={this.handlePassword}
+                className="login-input"
+                />
+            </label>
+            <br/>
+            <input type="submit" value={this.props.formType} className="login-submit"/>
+          </div>
         </form>
       </div>  
-    )
+    );
   }
 }
 
