2049518a8e167fd4b3d6810c4ba60eaf82181ec8
kristinamiller <kfm287@gmail.com>
2019-09-12 15:22:20 -0700

    started modal styling

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -54,10 +54,10 @@ class SessionForm extends React.Component {
     return(
       <div className="login-form-container">
         <form onSubmit={this.handleSubmit} className="login-form-box">
-          <h3>Please {this.props.formType}</h3>
-          <br/>
-          <h3>{this.renderErrors()}</h3>
+          
           <div className="login-form">
+            <h3>Please {this.props.formType}</h3>
+            <h3>{this.renderErrors()}</h3>
             <br/>
             <label>
               Email:
