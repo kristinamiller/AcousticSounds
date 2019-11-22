993d2f8a9cab7c825e7eee5b9439ee091d46282c
kristinamiller <kfm287@gmail.com>
2019-11-21 18:16:44 -0800

    cleaned up comment component

--- frontend/components/comments/comment_form.jsx
+++ frontend/components/comments/comment_form.jsx
@@ -26,19 +26,6 @@ class CommentForm extends React.Component {
   }
 
 
-  
-  // renderErrors() {
-  //   return (
-  //     <ul>
-  //       {this.props.errors.map((error, i) => (
-  //         <li key={`error-${i}`}>
-  //           {error}
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   )
-  // }
-
   render() {
 
 
